cf14b60219c335828fcd0c620bb4ceebcafd21ae
Emmanuel Joshy <92877747+Eman-69@users.noreply.github.com>
2026-09-25 18:35:34 +0530

    Add: longest-consecutive-sequence - submission-7

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-7.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Arrays.sort(nums);
+        int count=0;
+        int i=0;
+        int maxCount=0;
+        if(nums.length==0)
+            return 0;
+
+        while(i<nums.length-1)
+        {            
+            while(nums[i+1]-nums[i]==0&&i<nums.length-2)
+            {
+                i++;
+            }
+            if(nums[i+1]-nums[i]==1)
+            {
+                i++;
+                if(count==0)
+                count+=2;
+                else
+                    count++;
+            }
+
+            else{
+                maxCount=Math.max(maxCount,count);
+                count=0;
+                i++;
+            }
+           // System.out.println(maxCount+" "+nums[i]+" "+i);
+        }
+        maxCount=Math.max(maxCount,count); 
+        return maxCount==0?1:maxCount;
+    }
+}
